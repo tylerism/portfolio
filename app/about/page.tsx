4979641c3aa875afb587dc293d1684c5a40cc883
tyler <tylerism@gmail.com>
2024-05-29 17:22:12 -0500

    adding repo

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,9 +4,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <div className="center-logo">
-          <img src="https://i.imgur.com/Bub9zuq.png"/>
-        </div>
+      <div className="typewriter">
+        <h1>The cat and the hat, about.</h1>
+      </div>
       </div>
     </main>
   );
