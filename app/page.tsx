5ad914553c421351ad1e0dabece8779573137787
tyler <tylerism@gmail.com>
2024-05-30 09:16:00 -0500

    add pulse

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,8 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <div className="center-logo">
-          <img src="https://i.imgur.com/g1ibqtr.png"/>
+          <img className="center-logo-fg" src="https://i.imgur.com/WTJ88Hx.png"/>
+          <img className="center-logo-bg" src="https://i.imgur.com/ZA7drrf.png"/>
         </div>
       </div>
     </main>
