a5f1f2be1c6db980dade37d1baa7127618d471f5
tyler <tylerism@gmail.com>
2024-05-30 10:22:24 -0500

    mobile friendly

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <div className="center-logo">
-          <img className="center-logo-fg" src="https://i.imgur.com/WTJ88Hx.png"/>
-          <img className="center-logo-bg" src="https://i.imgur.com/ZA7drrf.png"/>
+    <main>
+      <div>
+        <div className="center-logo-container">
+          <div className="center-logo">
+            <img className="center-logo-fg" src="https://i.imgur.com/WTJ88Hx.png"/>
+            <img className="center-logo-bg" src="https://i.imgur.com/ZA7drrf.png"/>
+          </div>
         </div>
       </div>
     </main>
