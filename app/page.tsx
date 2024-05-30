b877d4e77ac8ae4e3a0b1d3b65b801699f775402
tyler <tylerism@gmail.com>
2024-05-30 12:43:37 -0500

    coming soon

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ export default function Home() {
           <div className="center-logo">
             <img className="center-logo-fg" src="https://i.imgur.com/WTJ88Hx.png"/>
             <img className="center-logo-bg" src="https://i.imgur.com/ZA7drrf.png"/>
+            <p>( COMING SOON )</p>
           </div>
         </div>
       </div>
